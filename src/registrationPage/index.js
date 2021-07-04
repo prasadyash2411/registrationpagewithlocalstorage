775dd762552385466e1b61e3ef11576f939efdfa
yash prasad <pyash@athenahealth.com>
2021-07-04 23:02:32 +0530

    final fixes

--- src/registrationPage/index.js
+++ src/registrationPage/index.js
@@ -1,37 +1,21 @@
-import "../styles.css";
-
-const submitButton = document.getElementById("submit");
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("vscode");
-  localStorage.getItem();
-});
 
-if (document.readyState !== "loading") {
-  console.log("vscode");
-
-  var keys = Object.keys(localStorage), //taking out all the keys that are there in the local storage
-    i = keys.length; //6
-  console.log("keys", keys);
   let stringifiedDetailsOfPeople, detailsOfPeople;
 
-  // 6 to 0
   Object.keys(localStorage).forEach((key) => {
-    //i==2
-    if (key.match(/userDetails/g)) {
-      //we only care about keys that start with userDetails
-      //this is called regex matching
+
       stringifiedDetailsOfPeople = localStorage.getItem(key);
       console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
       detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
       console.log("details", detailsOfPeople);
 
       addNewLineElement(detailsOfPeople);
-    }
   });
-}
-// const listOfPeople = []
-submitButton.addEventListener("click", (e) => {
+})
+function addNewUser (e) {
+  console.log('submitted ')
   e.preventDefault();
   const emailId = document.getElementById("email").value;
   const name = document.getElementById("name").value;
@@ -41,11 +25,9 @@ submitButton.addEventListener("click", (e) => {
       emailId: emailId //unique
     };
     localStorage.setItem("userDetails" + emailId, JSON.stringify(object));
-    // localStorage.setItem("userDetailEmail" + emailId, emailId);
-    // listOfPeople.push(object)
     addNewLineElement(object);
   }
-});
+};
 
 function addNewLineElement(object) {
   const ul = document.getElementById("listOfPeople");
@@ -75,7 +57,6 @@ function addNewLineElement(object) {
   a.value = "delete";
   a.addEventListener("click", () => {
     localStorage.removeItem("userDetails" + object.emailId);
-    // axios.delete(`${apiendpoint}/registeruser/${object._id}`);
     li.remove();
   });
   a.className = "delete";
